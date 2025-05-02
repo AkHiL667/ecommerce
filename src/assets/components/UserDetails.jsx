42ffe65aa35e350ba1b8062c8f58df720ee6a1e1
Akhil <konda.akhil456@gmail.com>
2025-05-03 02:34:05 +0530

    completed

--- src/assets/components/UserDetails.jsx
+++ src/assets/components/UserDetails.jsx
@@ -1,28 +1,35 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import { userContext } from '../utils/Context'
 import axios from '../utils/Axios'
 import Loading from './Loading'
 
 function UserDetails() {
+  const navigate = useNavigate()
   const[val, setVal] = useContext(userContext)
   const req = useParams()
   const needid = req.id
- let [value, setvalue] = useState(null)
-//  let getData = async ()=>{
-//   try {
-//     let data = await axios('/products/'+needid)
-//     setVal(data.data)
-//   } catch (error) {
-//     console.log(error)
-//   }
-//  }
- useEffect(()=>{
-  if (!value){
-  setvalue(val.filter((p)=> p.id == needid)[0])
+  let [value, setvalue] = useState(null)
+  //  let getData = async ()=>{
+    //   try {
+      //     let data = await axios('/products/'+needid)
+      //     setVal(data.data)
+      //   } catch (error) {
+        //     console.log(error)
+        //   }
+        //  }
+        useEffect(()=>{
+          if (!value){
+            setvalue(val.filter((p)=> p.id == needid)[0])
   }
 },[])
 
+const deleteHandler = (id)=>{
+  const filteredProducts = val.filter(p => p.id != id)
+  setVal(filteredProducts)
+  localStorage.setItem("val",JSON.stringify(filteredProducts))
+  navigate("/")
+}
   return ( value? 
 
     <div className='flex h-screen w-[80%] py-[10%] gap-5 m-auto'>
@@ -34,8 +41,8 @@ function UserDetails() {
         <p className='text-xs w-[70%] '>{value.description}</p>
         <div className='flex gap-2 mt-5'>
 
-        <Link className='px-2 bg-blue-500 w-18 text-center text-white font-bold rounded-md'>Edit</Link>
-        <Link className='px-2 bg-red-500 text-white font-bold w-18 text-center rounded-md'>Delete</Link>
+        {/* <Link className='px-2 bg-blue-500 w-18 text-center text-white font-bold rounded-md'>Edit</Link> */}
+        <button onClick={()=>deleteHandler(value.id)} className='px-2 bg-red-500 text-white font-bold w-18 text-center rounded-md'>Delete</button>
         </div>
 
       </div>
